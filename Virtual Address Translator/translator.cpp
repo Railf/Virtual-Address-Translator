67624b486378b3301f352cee440f0eceb9904ff9
Ralph McCracken, III <Ralph@McCracken.work>
2017-12-05 00:12:19 -0600

    Initial capabilities: Virtual Page Number, Page Offset, TLB Tag, and TLB Index.

---
+++ Virtual Address Translator/translator.cpp
@@ -0,0 +1,27 @@
+//
+//  translator.cpp
+//  Virtual Address Translator
+//
+//  Created by Railf on 12/5/17.
+//  Copyright © 2017 Railf. All rights reserved.
+//
+
+#include <iostream>
+#include "virtual-tlb.h"
+
+int main() {
+    unsigned int va = 0;
+    
+    while (va != 1)
+    {
+        std::cout << "Virtual address? ";
+        std::cin >> std::hex >> va;
+        
+        vir.x = va;
+        
+        std::cout << "Vir Page # : " << std::dec << vir.table.page << '\n';
+        std::cout << "Page Offset: " << std::hex << vir.TLB.offset << '\n';
+        std::cout << "TLB Tag    : " << std::dec << vir.TLB.tag    << '\n';
+        std::cout << "TLB Index  : " << std::dec << vir.TLB.index  << '\n' << '\n';
+    }
+}
---
+++ Virtual Address Translator/virtual-tlb.h
@@ -0,0 +1,32 @@
+//
+//  virtual-tlb.h
+//  Virtual Address Translator
+//
+//  Created by Railf on 12/5/17.
+//  Copyright © 2017 Railf. All rights reserved.
+//
+
+#ifndef virtual_tlb_h
+#define virtual_tlb_h
+
+union
+{
+    unsigned int x;
+    
+    struct
+    {
+        unsigned int offset:13;
+        unsigned int index:1;
+        unsigned int tag:6;
+    } TLB;
+    
+    
+    struct
+    {
+        unsigned int offset:13;
+        unsigned int page:7;
+    }table;
+    
+} vir;
+
+#endif /* virtual_tlb_h */
