6772418687a8454c0230e27090f806b7c817413c
Ralph McCracken, III <Ralph@McCracken.work>
2017-12-05 00:14:37 -0600

    Table files added.

---
+++ Virtual Address Translator/page-table.h
@@ -0,0 +1,13 @@
+//
+//  page-table.h
+//  Virtual Address Translator
+//
+//  Created by Railf on 12/5/17.
+//  Copyright © 2017 Railf. All rights reserved.
+//
+
+#ifndef page_table_h
+#define page_table_h
+
+
+#endif /* page_table_h */
---
+++ Virtual Address Translator/tlb-table.h
@@ -0,0 +1,13 @@
+//
+//  tlb-table.h
+//  Virtual Address Translator
+//
+//  Created by Railf on 12/5/17.
+//  Copyright © 2017 Railf. All rights reserved.
+//
+
+#ifndef tlb_table_h
+#define tlb_table_h
+
+
+#endif /* tlb_table_h */
